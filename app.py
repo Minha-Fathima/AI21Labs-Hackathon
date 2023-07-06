b476a5d7d82329f43b0a097cb0dd3801e7dfc943
Minha Fathima Avoor <113610393+Minha-Fathima@users.noreply.github.com>
2023-07-07 03:25:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+import logging
+from flask import render_template, request, Flask
+from pdf_to_text import pdfToText
+from grammar_check import grammarCheck, getLineNo
+from extraction import Job_Title_Extraction, Skills_Extraction
+from skill_recommendation import skillRecommendation
+from certification_recommendation import certificateRecommendation
+
+app = Flask(__name__)
+# app.debug = True
+app.logger.setLevel(logging.DEBUG)
+
+
+# Set up the route for the home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
+    if file and file.filename.endswith(".pdf"):
+        text = pdfToText(file)
+
+        # grammer check
+        grammar_errors = grammarCheck(text)
+        line = getLineNo(text, grammar_errors)
+
+        job = Job_Title_Extraction(text)
+        skill = Skills_Extraction(text)
+
+        # skill recommendation
+        skillRec = skillRecommendation(job, skill)
+        skillRec = skillRec.split(",")
+
+        # certification recommendation
+        certificateRec = certificateRecommendation(job)
+        certificateRec = certificateRec.split(",")
+
+        return render_template(
+            "index.html",
+            text=text,
+            grammar_errors=grammar_errors,
+            line=line,
+            certificateRec=certificateRec,
+            skillRec=skillRec,
+        )
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
